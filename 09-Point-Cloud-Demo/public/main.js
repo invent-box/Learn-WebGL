b99d9f8f84feea33b7af0ff524e78b019ef0a3c6
Samir Ghosh <hello@samir.tech>
2020-11-28 20:34:23 -0800

    fix: gl_PointSize must be declared for FF

--- 09-Point-Cloud-Demo/public/main.js
+++ 09-Point-Cloud-Demo/public/main.js
@@ -38,6 +38,7 @@ uniform mat4 matrix;
 void main() {
     vColor = vec3(position.xy, 1);
     gl_Position = matrix * vec4(position, 1);
+    gl_PointSize = 1.0;
 }
 `);
 gl.compileShader(vertexShader);
